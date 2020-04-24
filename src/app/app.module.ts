eeb1432cafdaaefa1b13d6c683daeabfc1af0d26
alexAbela <s175124@student.dtu.dk>
2020-04-24 14:11:36 +0200

    virkede heller ikke fuck

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule } from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import {ReactiveFormsModule} from '@angular/forms';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FrameComponent } from './components/frame/frame.component';
-import { DropdownComponent } from './components/frame/dropdown/dropdown.component';
-import { LoginComponent } from './components/login/login.component';
-import { DatabaseConnecterComponent } from './database-connecter/database-connecter.component';
-import { UsersComponent } from './components/users/users.component';
-import { UsersListComponent } from './components/users/users-list/users-list.component';
-import { UsersItemComponent } from './components/users/users-list/users-item/users-item.component';
-import { UsersDetailComponent } from './components/users/users-detail/users-detail.component';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './components/header/header.component';
+import {FrameComponent} from './components/frame/frame.component';
+import {DropdownComponent} from './components/frame/dropdown/dropdown.component';
+import {LoginComponent} from './components/login/login.component';
+import {DatabaseConnecterComponent} from './database-connecter/database-connecter.component';
+import {UsersComponent} from './components/users/users.component';
+import {UsersListComponent} from './components/users/users-list/users-list.component';
+import {UsersItemComponent} from './components/users/users-list/users-item/users-item.component';
+import {UsersDetailComponent} from './components/users/users-detail/users-detail.component';
 
 
 @NgModule({
@@ -31,9 +32,11 @@ import { UsersDetailComponent } from './components/users/users-detail/users-deta
   imports: [
     BrowserModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
