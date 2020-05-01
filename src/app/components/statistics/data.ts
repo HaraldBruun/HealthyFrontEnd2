7a4157c4e792c0acd7b500fd9f5ca8be08363e32
HaraldBruun <s185014@student.dtu.dk>
2020-05-01 03:49:46 +0200

    Updating charts dynamically now working.

---
+++ src/app/components/statistics/data.ts
@@ -0,0 +1,485 @@
+export var single = [
+    {
+      'name': 'Dani',
+      'value': 8940000
+    },
+    {
+      'name': 'Toma',
+      'value': 5000000
+    },
+    {
+      'name': 'Alix',
+      'value': 7200000
+    }
+  ];
+
+/*
+
+  /*{
+    'username': 'healthy12@gmail.com',
+    'password': '123123',
+    'firstTimeLoggedIn': true,
+    'physique': {
+      'height': 0.0,
+      'weight': 0.0,
+      'activityLevel': 0
+    },
+    'personalInfo': {
+      'firstName': 'Penis',
+      'lastName': '',
+      'gender': 'n',
+      'dateOfBirth': 0,
+      'zipCode': 0,
+      'age': 50
+    },
+    'experience': {
+      'level': 1,
+      'nutritionXP': 0,
+      'activityXP': 0,
+      'socialXP': 0,
+      'ticket': 0,
+      'totalXP': 0,
+      'xpforCalories': false,
+      'xpforProtein': false,
+      'xpforCarbs': false,
+      'xpforFat': false
+    },
+    'meals': [],
+    'friends': [],
+    'activities': [],
+    'rewards': [],
+    'uid': 'M7QfI790I9QB4W5PstSMndRU7IA3'
+  }, {
+    'username': 'r@www.dk',
+    'password': 'qwerty',
+    'firstTimeLoggedIn': false,
+    'physique': {
+      'height': 123.0,
+      'weight': 7.0,
+      'activityLevel': 3
+    },
+    'personalInfo': {
+      'firstName': '',
+      'lastName': '',
+      'gender': 'male',
+      'dateOfBirth': 1674946800000,
+      'zipCode': 0,
+      'age': -3
+    },
+    'experience': {
+      'level': 2,
+      'nutritionXP': 0,
+      'activityXP': 0,
+      'socialXP': 0,
+      'ticket': 0,
+      'totalXP': 0,
+      'xpforCalories': true,
+      'xpforProtein': true,
+      'xpforCarbs': false,
+      'xpforFat': true
+    },
+    'meals': [{
+      'name': 'Esrom, 45+',
+      'calories': 331.0,
+      'protein': 23.7,
+      'carbs': 0.0,
+      'fat': 25.7,
+      'typeOfMeal': 'Morgenmad',
+      'date': 1580335153065
+    }, {
+      'name': 'Banan, rå',
+      'calories': 93.0,
+      'protein': 1.1,
+      'carbs': 19.7,
+      'fat': 0.2,
+      'typeOfMeal': 'Frokost',
+      'date': 1580335160755
+    }, {
+      'name': 'Ymer naturel',
+      'calories': 72.0,
+      'protein': 6.0,
+      'carbs': 3.4,
+      'fat': 3.6,
+      'typeOfMeal': 'Aftensmad',
+      'date': 1580335166664
+    }, {
+      'name': 'Persille, rå',
+      'calories': 48.0,
+      'protein': 3.7,
+      'carbs': 1.2,
+      'fat': 1.1,
+      'typeOfMeal': 'Aftensmad',
+      'date': 1580335177696
+    }, {
+      'name': 'Dild, rå',
+      'calories': 48.0,
+      'protein': 3.5,
+      'carbs': 4.9,
+      'fat': 1.1,
+      'typeOfMeal': 'Snacks',
+      'date': 1580335186531
+    }, {
+      'name': 'Danablu 60+',
+      'calories': 407.0,
+      'protein': 15.9,
+      'carbs': 0.1,
+      'fat': 37.4,
+      'typeOfMeal': 'Morgenmad',
+      'date': 1580335249521
+    }, {
+      'name': 'Pop Corn (poppede majskærner)',
+      'calories': 489.0,
+      'protein': 9.6,
+      'carbs': 53.7,
+      'fat': 24.3,
+      'typeOfMeal': 'Morgenmad',
+      'date': 1580335257178
+    }, {
+      'name': 'Fisk',
+      'calories': 250.0,
+      'protein': 8.0,
+      'carbs': 32.0,
+      'fat': 10.0,
+      'typeOfMeal': 'Morgenmad',
+      'date': 1580335262703
+    }, {
+      'name': 'Kartoffel, uspec., rå',
+      'calories': 77.0,
+      'protein': 2.0,
+      'carbs': 17.9,
+      'fat': 0.3,
+      'typeOfMeal': 'Frokost',
+      'date': 1580335269486
+    }, {
+      'name': 'Hornfisk, rå',
+      'calories': 107.0,
+      'protein': 19.5,
+      'carbs': 0.0,
+      'fat': 3.2,
+      'typeOfMeal': 'Aftensmad',
+      'date': 1580335275759
+    }, {
+      'name': 'Rugmel, groft, fuldkornsrugmel',
+      'calories': 333.0,
+      'protein': 9.4,
+      'carbs': 54.8,
+      'fat': 2.3,
+      'typeOfMeal': 'Aftensmad',
+      'date': 1580335347920
+    }, {
+      'name': 'Laks, atlantisk, vild, rå',
+      'calories': 159.0,
+      'protein': 18.4,
+      'carbs': 0.0,
+      'fat': 7.5,
+      'typeOfMeal': 'Aftensmad',
+      'date': 1580335355418
+    }],
+    'friends': [],
+    'activities': ['Ridning', 'Fodbold', 'bs'],
+    'rewards': [{
+      'name': 'Smoothie',
+      'tier': 1,
+      'resource': 2131165325,
+      'redeemed': false
+    }],
+    'uid': 'Nsy3qLRUCFWznBD8HTyqT2FB5IC3'
+  }, {
+    'username': 'anton123',
+    'password': '123123',
+    'firstTimeLoggedIn': false,
+    'physique': {
+      'height': 180.0,
+      'weight': 80.0,
+      'activityLevel': 1
+    },
+    'personalInfo': {
+      'firstName': 'Anton',
+      'lastName': 'Hansen',
+      'gender': 'male',
+      'dateOfBirth': 123123,
+      'zipCode': 2820,
+      'age': 0
+    },
+    'experience': {
+      'level': 1,
+      'nutritionXP': 1,
+      'activityXP': 1,
+      'socialXP': 1,
+      'ticket': 1,
+      'totalXP': 0,
+      'xpforCalories': false,
+      'xpforProtein': false,
+      'xpforCarbs': false,
+      'xpforFat': false
+    },
+    'meals': [],
+    'friends': [],
+    'activities': [],
+    'rewards': [],
+    'uid': 'TestUser123'
+  }, {
+    'username': 'gruppe7@gmail.com',
+    'password': '123123',
+    'firstTimeLoggedIn': true,
+    'physique': {
+      'height': 0.0,
+      'weight': 0.0,
+      'activityLevel': 0
+    },
+    'personalInfo': {
+      'firstName': '',
+      'lastName': '',
+      'gender': 'n',
+      'dateOfBirth': 0,
+      'zipCode': 0,
+      'age': 50
+    },
+    'experience': {
+      'level': 1,
+      'nutritionXP': 0,
+      'activityXP': 0,
+      'socialXP': 0,
+      'ticket': 0,
+      'totalXP': 0,
+      'xpforCalories': false,
+      'xpforProtein': false,
+      'xpforCarbs': false,
+      'xpforFat': false
+    },
+    'meals': [],
+    'friends': [],
+    'activities': [],
+    'rewards': [],
+    'uid': null
+  }, {
+    'username': '123@123.dk',
+    'password': '!!!!!!',
+    'firstTimeLoggedIn': false,
+    'physique': {
+      'height': 111.0,
+      'weight': 111.0,
+      'activityLevel': 1
+    },
+    'personalInfo': {
+      'firstName': '',
+      'lastName': '',
+      'gender': 'male',
+      'dateOfBirth': 1579734000000,
+      'zipCode': 0,
+      'age': 0
+    },
+    'experience': {
+      'level': 1,
+      'nutritionXP': 0,
+      'activityXP': 0,
+      'socialXP': 0,
+      'ticket': 0,
+      'totalXP': 0,
+      'xpforCalories': false,
+      'xpforProtein': false,
+      'xpforCarbs': false,
+      'xpforFat': false
+    },
+    'meals': [],
+    'friends': [],
+    'activities': [],
+    'rewards': [],
+    'uid': 'n60Y2YuDYCgEDYBK98lvAT0p0Ot1'
+  }, {
+    'username': 'healthy14@gmail.com',
+    'password': '123123',
+    'firstTimeLoggedIn': false,
+    'physique': {
+      'height': 35.0,
+      'weight': 68.0,
+      'activityLevel': 5
+    },
+    'personalInfo': {
+      'firstName': 'Anders',
+      'lastName': 'Hansen',
+      'gender': 'male',
+      'dateOfBirth': 2116278000000,
+      'zipCode': 0,
+      'age': -17
+    },
+    'experience': {
+      'level': 2,
+      'nutritionXP': 5,
+      'activityXP': 0,
+      'socialXP': 5,
+      'ticket': 0,
+      'totalXP': 10,
+      'xpforCalories': true,
+      'xpforProtein': true,
+      'xpforCarbs': false,
+      'xpforFat': true
+    },
+    'meals': [{
+      'name': 'Fiskeolie, hydrogeneret',
+      'calories': 899.0,
+      'protein': 0.0,
+      'carbs': 0.0,
+      'fat': 99.9,
+      'typeOfMeal': 'Morgenmad',
+      'date': 1579798623855
+    }, {
+      'name': 'Fiskeolie, hydrogeneret',
+      'calories': 899.0,
+      'protein': 0.0,
+      'carbs': 0.0,
+      'fat': 99.9,
+      'typeOfMeal': 'Morgenmad',
+      'date': 1579798634543
+    }, {
+      'name': 'Fiskeolie, hydrogeneret',
+      'calories': 899.0,
+      'protein': 0.0,
+      'carbs': 0.0,
+      'fat': 99.9,
+      'typeOfMeal': 'Morgenmad',
+      'date': 1579798639600
+    }, {
+      'name': 'Kylling, bryst (filet), kogt, pålæg',
+      'calories': 113.0,
+      'protein': 20.6,
+      'carbs': 0.4,
+      'fat': 3.2,
+      'typeOfMeal': 'Frokost',
+      'date': 1579806723914
+    }, {
+      'name': 'Bagegær, presset, rå',
+      'calories': 114.0,
+      'protein': 8.4,
+      'carbs': 0.0,
+      'fat': 1.9,
+      'typeOfMeal': 'Morgenmad',
+      'date': 1579806730125
+    }, {
+      'name': 'And, kød og skind, rå',
+      'calories': 400.0,
+      'protein': 11.5,
+      'carbs': 0.0,
+      'fat': 39.3,
+      'typeOfMeal': 'Morgenmad',
+      'date': 1579806736693
+    }, {
+      'name': 'Kødboller, konserves',
+      'calories': 126.0,
+      'protein': 7.1,
+      'carbs': 2.9,
+      'fat': 9.6,
+      'typeOfMeal': 'Morgenmad',
+      'date': 1579806749473
+    }, {
+      'name': 'Kikærter, tørrede, rå',
+      'calories': 366.0,
+      'protein': 20.4,
+      'carbs': 45.9,
+      'fat': 6.9,
+      'typeOfMeal': 'Morgenmad',
+      'date': 1579806778763
+    }, {
+      'name': 'Kikærter, tørrede, rå',
+      'calories': 366.0,
+      'protein': 20.4,
+      'carbs': 45.9,
+      'fat': 6.9,
+      'typeOfMeal': 'Morgenmad',
+      'date': 1579806782903
+    }, {
+      'name': 'Kikærter, tørrede, rå',
+      'calories': 366.0,
+      'protein': 20.4,
+      'carbs': 45.9,
+      'fat': 6.9,
+      'typeOfMeal': 'Morgenmad',
+      'date': 1579806789019
+    }, {
+      'name': 'Kikærter, tørrede, rå',
+      'calories': 366.0,
+      'protein': 20.4,
+      'carbs': 45.9,
+      'fat': 6.9,
+      'typeOfMeal': 'Morgenmad',
+      'date': 1579806793778
+    }, {
+      'name': 'Kikærter, tørrede, rå',
+      'calories': 366.0,
+      'protein': 20.4,
+      'carbs': 45.9,
+      'fat': 6.9,
+      'typeOfMeal': 'Morgenmad',
+      'date': 1579806797805
+    }],
+    'friends': ['o61l598qIweh3jemFFf69nsj1zj2', '2yvQJP5zfsPF81bMdy9FNt5QpkM2'],
+    'activities': ['Fodbold', 'Håndbold', 'Basketball'],
+    'rewards': [{
+      'name': 'Smoothie',
+      'tier': 1,
+      'resource': 2131165325,
+      'redeemed': false
+    }, {
+      'name': '10 Kr i Kantinen',
+      'tier': 1,
+      'resource': 2131165382,
+      'redeemed': false
+    }, {
+      'name': 'Raw Bar',
+      'tier': 1,
+      'resource': 2131165327,
+      'redeemed': false
+    }, {
+      'name': 'Juice',
+      'tier': 1,
+      'resource': 2131165334,
+      'redeemed': false
+    }, {
+      'name': 'Smoothie',
+      'tier': 1,
+      'resource': 2131165325,
+      'redeemed': false
+    }, {
+      'name': 'Ti XP',
+      'tier': 1,
+      'resource': 2131165381,
+      'redeemed': false
+    }, {
+      'name': 'Ti XP',
+      'tier': 1,
+      'resource': 2131165381,
+      'redeemed': false
+    }, {
+      'name': 'Fem XP',
+      'tier': 1,
+      'resource': 2131165380,
+      'redeemed': false
+    }, {
+      'name': 'Raw Bar',
+      'tier': 1,
+      'resource': 2131165327,
+      'redeemed': false
+    }, {
+      'name': 'Trænings Elastik',
+      'tier': 2,
+      'resource': 2131165344,
+      'redeemed': false
+    }, {
+      'name': 'Smoothie',
+      'tier': 1,
+      'resource': 2131165325,
+      'redeemed': false
+    }, {
+      'name': 'Smoothie',
+      'tier': 1,
+      'resource': 2131165325,
+      'redeemed': false
+    }, {
+      'name': '10 Kr i Kantinen',
+      'tier': 1,
+      'resource': 2131165382,
+      'redeemed': false
+    }],
+    'uid': 'o61l598qIweh3jemFFf69nsj1zj2'
+  }];
+*/
---
+++ src/app/components/statistics/statistics.component.ts
@@ -0,0 +1,78 @@
+import {Component, OnInit, NgModule} from '@angular/core';
+import {NgxChartsModule} from '@swimlane/ngx-charts';
+import {BrowserModule} from '@angular/platform-browser';
+import {single} from './data';
+import {UsersService} from '../../shared/users.service';
+import {DatabaseService} from '../../database.service';
+import {Pupil} from '../../shared/user.model';
+
+function setSingleSingle(id: number, name: string, value: number) {
+  single[id].name = name;
+  single[id].value = value;
+}
+
+@Component({
+  selector: 'app-statistics',
+  templateUrl: './statistics.component.html',
+  styleUrls: ['./statistics.component.css']
+})
+export class StatisticsComponent implements OnInit {
+  single: any[];
+  users: Pupil[];
+  view: any[] = [700, 400];
+  totalActivityXP = 0;
+  totalSocialXP = 0;
+  totalNutritionXP = 0;
+
+  // Options
+  gradient = true;
+  showLegend = true;
+  showLabels = true;
+  isDoughnut = false;
+  legendPosition = 'below';
+
+  colorScheme = {
+    domain: ['#a42036', '#33a125', '#4945c7']
+  };
+
+
+  constructor(private usersService: UsersService, private databaseService: DatabaseService) {
+    Object.assign(this, {single});
+  }
+
+  onSelect(data): void {
+    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+  }
+
+  onActivate(data): void {
+    console.log('Activate', JSON.parse(JSON.stringify(data)));
+  }
+
+  onDeactivate(data): void {
+    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
+  }
+
+  ngOnInit(): void {
+    this.databaseService.getAllUsers();
+    this.users = this.usersService.getUsers();
+    this.usersService.usersChanged.subscribe(
+      (users: Pupil[]) => {
+        this.single == users;
+        for (let user in users) {
+          console.log(users[user]);
+          this.totalActivityXP += users[user].experience.activityXP.valueOf();
+          this.totalNutritionXP += users[user].experience.nutritionXP.valueOf();
+          this.totalSocialXP += users[user].experience.socialXP.valueOf();
+        }
+        console.log('TotalActivityXP = ' + this.totalActivityXP);
+        console.log('TotalNutritionXP = ' + this.totalNutritionXP);
+        console.log('TotalSocialXP = ' + this.totalSocialXP);
+        setSingleSingle(0, 'ActivityXP', this.totalActivityXP);
+        setSingleSingle(1, 'NutritionXP', this.totalNutritionXP);
+        setSingleSingle(2, 'SocialXP', this.totalSocialXP);
+        // VERY FUCKING IMPORTANT FOR UPDATING
+        this.single = [...this.single];
+      }
+    );
+  }
+}
